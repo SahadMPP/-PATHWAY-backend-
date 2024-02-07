676aebd5d7af06b8d8bde91b6e953af289ea7cc9
Sahad Mp <muhdsahad4916@gmail.com>
2024-02-07 14:30:20 +0530

    change in teacher model

--- user_teacher.js
+++ user_teacher.js
@@ -2,57 +2,62 @@ const mongoose = require("mongoose");
 
 let teacherDataSchema = new mongoose.Schema({
 
-    name: {
-        type: String,
-        required: true
-      },
-      profileImage: {
-        type: String,
-      },
-      email: {
-        type: String,
-        required: true
-      },
-      password: {
-        type: String,
-        
-      },
-      mobNumber: {
-        type: String
-      },
-      universityName: {
-        type: String
-      },
-      universityPlace: {
-        type: String
-      },
-      universityState: {
-        type: String
-      },
-      experience: {
-        type: String
-      },
-      officerName: {
-        type: String
-      },
-      certificates: {
-        type: Map,
-        of: String
-      },
-      signatureImage: {
-        type: String
-      },
-      active: {
-        type: Boolean,
-        default: true
-      },
-      subjects: {
-        type: Map,
-        of: {
-            type: Boolean,
-            default: false
-          }
-      }
+  name: {
+    type: String,
+    required: true
+  },
+  profileImage: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  appledSubject: {
+    type: String,
+  },
+  appledStatus: {
+    type: Boolean,
+  },
+  password: {
+    type: String,
+  },
+  mobNumber: {
+    type: String
+  },
+  universityName: {
+    type: String
+  },
+  universityPlace: {
+    type: String
+  },
+  universityState: {
+    type: String
+  },
+  experience: {
+    type: String
+  },
+  officerName: {
+    type: String
+  },
+  certificates: {
+    type: Map,
+    of: String
+  },
+  signatureImage: {
+    type: String
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  subjects: {
+    type: Map,
+    of: {
+      type: Boolean,
+      default: false
+    }
+  }
 })
 
-module.exports = mongoose.model("teacher",teacherDataSchema);
+module.exports = mongoose.model("teacher", teacherDataSchema);
